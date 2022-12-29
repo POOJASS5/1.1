60c508febf68b0fc248ccdafd2ea13c7520c01df
poojaANUP <poojaanup522@gmail.com>
2022-12-29 20:07:53 +0530

    third

---
+++ main8.js
@@ -0,0 +1,40 @@
+//using call
+//var obj={num:2};
+//var addToThis=function(a){
+//    return this.num+a;
+//};
+//console.log(addToThis.call(obj, 3));
+
+
+
+
+//using apply
+//var obj={num:2};
+//var obj2={num:5};
+//var addToThis=function(a, b, c){
+    //return this.num+a+b+c;
+//};
+//var arr=[1,2,3];
+//console.log(addToThis.apply(obj,arr));
+//console.log(addToThis.apply(obj2,arr));
+
+
+
+//using bind
+//var obj={num:2};
+//var obj2={num:5};
+//var addToThis=function(a, b, c){
+    //return this.num+a+b+c;
+//};
+//var arr=[1,2,3];
+//var bound=addToThis.bind(obj);
+//console.log(bound(1,2,3));
+
+
+var obj={num:2};
+var functionName=function(arg1,arg2,arg3){}
+functionName.call(obj,arg1,arg2,arg3);
+functionName.apply(obj,arg1,arg2,arg3);
+var bound=functionName.bind(obj);
+console.log(bound(arg1,arg2,arg3));
+
